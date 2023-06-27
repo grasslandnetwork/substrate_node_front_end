bbd3c41a10ca958b4d8774108277f42fd414e8c9
George Batalinski <george.batalinski@gmail.com>
2023-06-27 09:23:39 -0400

    setup modal

--- src/Map.js
+++ src/Map.js
@@ -1,8 +1,17 @@
-
+import { Modal, Button } from 'semantic-ui-react'
 
 
 export default function Main(props) {  
     return (
-      <div>It works</div>
+        <Modal trigger={<Button>Show Metadata</Button>}>
+        <Modal.Header>Runtime Metadata</Modal.Header>
+        <Modal.Content scrolling>
+          <Modal.Description>
+            <pre>
+              <code>It works</code>
+            </pre>
+          </Modal.Description>
+        </Modal.Content>
+      </Modal>
     )
 }
